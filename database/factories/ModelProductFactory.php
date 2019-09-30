b8a08b3df107149d05c0d904dd1409406631c0e8
hussain <root@hussain.com>
2019-10-01 00:10:32 +0530

    seeding part created for Product and Review Model

--- database/factories/ModelProductFactory.php
+++ database/factories/ModelProductFactory.php
@@ -1,11 +1,17 @@
 <?php
 
+
 use Faker\Generator as Faker;
 
 /* @var Illuminate\Database\Eloquent\Factory $factory */
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(100,1000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2,30)
     ];
 });
