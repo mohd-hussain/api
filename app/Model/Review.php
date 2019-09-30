b71ed325fe92c946d64752002d61638003af05a6
hussain <root@hussain.com>
2019-10-01 00:30:39 +0530

    Relationship is created for Product and Review Model

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,15 @@
 
 namespace App\Model;
 
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product(){
+
+        return $this->belongsTo(Product::class);
+    }
+
+
 }
