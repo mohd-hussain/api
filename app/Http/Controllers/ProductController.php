35ec894bfbf6a8e27d3d027f502c0b8163ddf4d0
hussain <root@hussain.com>
2019-10-08 16:26:55 +0530

    Product Update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -88,7 +88,18 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        // return $request->all();
+        // return $product;
+
+        $request['detail'] = $request->description;
+        unset($request['description']);
+
+        $product->update($request->all());
+
+        return response(
+            [
+                'data' => new ProductResource($product)
+            ],201);
     }
 
     /**
